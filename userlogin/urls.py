9bb810d3008b91bddbdcdb0d32c671976c6c29fe
Bnguy0926 <73381026+Bnguy0926@users.noreply.github.com>
2022-10-15 16:40:11 -0700

    Home Page and Static File Created
    Homepage HTML and CSS file created!

---
+++ userlogin/urls.py
@@ -0,0 +1,10 @@
+from django.http import HttpRequest, HttpResponse
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.home),
+    path('about/', views.about),
+    path('customer/', views.customer),
+]
--- userlogin/views.py
+++ userlogin/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def home(request):
+    return render(request, 'userlogin/dashboard.html')
+
+def about(request):
+    return HttpResponse('About')
+
+def customer(request):
+    return HttpResponse('Customer')
