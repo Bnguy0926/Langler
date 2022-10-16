3ccda1c7e84132368ff260b995cdd8eef4e29c09
Bnguy0926 <73381026+Bnguy0926@users.noreply.github.com>
2022-10-15 19:59:55 -0700

    Reorganizing Files PT 2

--- langler/settings.py
+++ langler/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'langler.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR, 'templates'],
+        'DIRS': ['userlogin', 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
