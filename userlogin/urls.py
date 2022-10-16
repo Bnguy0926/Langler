c8cb4532efa5b3bef62d8ada353495d9cb0b8425
Bnguy0926 <73381026+Bnguy0926@users.noreply.github.com>
2022-10-16 01:00:30 -0700

    User Register and Login Authentication
    navbar, login, and register pages created and linked

--- userlogin/urls.py
+++ userlogin/urls.py
@@ -4,11 +4,12 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
+    path('', views.home, name = "home"),
     path('map/', views.map),
     path('about/', views.about),
     path('customer/', views.customer),
     # path('accounts/', include('django.contrib.auth.urls')),
     path('register/', views.registerPage, name="register"),
     path('login', views.loginPage, name="login"),
+    path('logout', views.loginPage, name="logout"),
 ]
--- userlogin/views.py
+++ userlogin/views.py
@@ -1,9 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import path
 
+from django.contrib.auth import authenticate, login, logout
+
+from django.contrib import messages
+
 # Create your views here.
+from .forms import CreateUserForm
 
 def home(request):
     return render(request, 'userlogin/dashboard.html')
@@ -18,17 +23,33 @@ def customer(request):
     return HttpResponse('Customer')
 
 def registerPage(request):
-    form = UserCreationForm()
-    context = {'form':form}
-
+    form = CreateUserForm()
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
-
+            user = form.cleaned_data.get('username')
+            messages.success(request, 'Account was created for' + user)
+            return redirect('login')
+    context = {'form':form}
     return render(request, 'userlogin/register.html', context)
 
 def loginPage(request):
-    form = UserCreationForm()
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username = username, password = password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.info(request, 'Username OR Password is incorrect')
+
     context = {}
-    return render(request, 'accounts/login.htlml', context)
+    return render(request, 'userlogin/login.html', context)
+
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
